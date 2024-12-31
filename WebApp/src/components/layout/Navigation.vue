b43dda2739b14aee123fd2a552676a7574e56ec7
lucvandeenen <lucvd@hotmail.nl>
2024-12-31 11:12:37 +0100

    made navigation menu responsive

--- WebApp/src/components/layout/Navigation.vue
+++ WebApp/src/components/layout/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-navigation-drawer permanent>
+  <v-navigation-drawer v-model="drawer" app :permanent="!$vuetify.display.mobile">
     <v-list>
       <v-list-item class="px-4 py-4">
         <span class="text-h6">🎉 Party App</span>
@@ -20,8 +20,16 @@
       <v-divider></v-divider>
       <user-menu />
     </template>
-
   </v-navigation-drawer>
+
+  <!-- Mobile Menu Button -->
+  <v-btn
+    icon
+    class="menu-btn"
+    @click="drawer = !drawer"
+  >
+    <v-icon>mdi-menu</v-icon>
+  </v-btn>
 </template>
 
 <script lang="ts">
@@ -31,7 +39,33 @@ import UserMenu from './UserMenu.vue'
 export default defineComponent({
   name: 'Navigation',
   components: {
-    UserMenu
-  }
+    UserMenu,
+  },
+  data() {
+    return {
+      drawer: false
+    }
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer
+    },
+  },
 })
 </script>
+
+<style scoped>
+.menu-btn {
+  position: absolute;
+  bottom: 16px;
+  right: 16px;
+  z-index: 10;
+  display: none;
+}
+
+@media (max-width: 600px) {
+  .menu-btn {
+    display: block;
+  }
+}
+</style>
